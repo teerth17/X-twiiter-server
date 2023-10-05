ce6e60513ce2b5cdf66b33b2233b8c9790312705
Teerth Patel <pteerth989@gmail.com>
2023-10-05 12:09:33 +0530

    initial commit

---
+++ src/services/jwt.ts
@@ -0,0 +1,20 @@
+import { User } from "@prisma/client";
+import { prismaClient } from "../clients/db";
+import JWT from "jsonwebtoken";
+
+
+const JWT_SECRET = "teerth17";
+
+class JWTService{
+    public static generateTokenForUser(user: User) {
+        const payload = {
+            id: user?.id,
+            email: user?.email,
+        };
+        const token = JWT.sign(payload, JWT_SECRET);
+        return token;
+
+    }
+}
+
+export default JWTService;
